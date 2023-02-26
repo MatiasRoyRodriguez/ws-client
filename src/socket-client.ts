a37a5ed852f65c592385584f64604d035fd69b50
Matías Roy Rodriguez <matiasrr980@gmail.com>
2023-02-26 11:33:31 -0300

    feat: detect connection and disconnection

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,4 +1,5 @@
 import { Manager } from 'socket.io-client';
+import { Socket } from 'socket.io-client/build/esm/socket';
 
 export const connectToServer = () => {
 
@@ -6,5 +7,23 @@ export const connectToServer = () => {
 	const manager = new Manager('localhost:3000/socket.io/socket.io.js')
 
 	const socket = manager.socket('/');
+	addListeners(socket);
+
+}
+
+
+const addListeners = (socket: Socket) => {
+
+	const serverStatusLabel = document.querySelector('#server-status')!;
+
+	socket.on('connect', () => {
+		serverStatusLabel.innerHTML = 'Connected';
+	});
+
+
+	socket.on('disconnect', () => {
+		serverStatusLabel.innerHTML = 'Disconnect';
+	});
+
 
 }
