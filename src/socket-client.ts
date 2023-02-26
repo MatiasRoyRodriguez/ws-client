b4ad362281c4197ca321adc5d1421609a376492c
Matías Roy Rodriguez <matiasrr980@gmail.com>
2023-02-26 13:06:15 -0300

    feat: take data from server

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -14,10 +14,11 @@ export const connectToServer = () => {
 
 const addListeners = (socket: Socket) => {
 
-	const serverStatusLabel = document.querySelector('#server-status')!;
 	const clientsUl = document.querySelector('#clients-ul')!;
 	const messageForm = document.querySelector<HTMLFormElement>('#message-form')!;
 	const messageInput = document.querySelector<HTMLInputElement>('#message-input')!;
+	const messagesUl = document.querySelector<HTMLUListElement>('#messages-ul')!;
+	const serverStatusLabel = document.querySelector('#server-status')!;
 
 
 
@@ -44,8 +45,27 @@ const addListeners = (socket: Socket) => {
 		event.preventDefault();
 		if (messageInput.value.trim().length <= 0) return;
 
-		socket.emit('message-from-client', { message: messageInput.value });
+		socket.emit('message-from-client', { fullName: 'test', message: messageInput.value });
 		messageInput.value = '';
+	});
+
+
+	socket.on('message-from-server', (payload: { fullName: string, message: string }) => {
+
+		const newMessage = `
+				<li>
+					<strong>
+						${payload.fullName}
+					</strong>
+					<span>
+						${payload.message}
+					</span>
+				</li>
+			`;
+		const li = document.createElement('li');
+		li.innerHTML = newMessage;
+		messagesUl.append(li);
+
 	});
 
 }
